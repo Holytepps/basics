12feb2965d98d44e5c1617eb79b60adb70fbb633
Bohdan Dobrotvor <holytepps@gmail.com>
2021-05-17 00:37:02 +0200

    fixed js

--- Basic Java Script/javascript/app.js
+++ Basic Java Script/javascript/app.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-console.log('Hello Wordl');
-console.log("object");
-=======
 const text = "Hello World!";
 console.log(text);
  const i = 10;
    for (x = 0; x < i; x++) {
      console.log(x);
    }
->>>>>>> javatest
